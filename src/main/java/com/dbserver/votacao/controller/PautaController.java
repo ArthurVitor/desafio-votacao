0851981a45e7795c6d0ed42aa8b9c3e3bf7988aa
ArthurVitor <arthurvitorgn@gmail.com>
2025-01-23 18:10:08 -0300

    feat: swagger configuration

---
+++ src/main/java/com/dbserver/votacao/controller/PautaController.java
@@ -0,0 +1,9 @@
+package com.dbserver.votacao.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/pauta")
+public class PautaController {
+}
