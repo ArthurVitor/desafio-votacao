858dabf6fa4b489140f8f4d9c9989096fa481606
ArthurVitor <arthurvitorgn@gmail.com>
2025-01-23 17:44:03 -0300

    initial commit

---
+++ src/main/java/com/dbserver/votacao/SicrediVotacaoApplication.java
@@ -0,0 +1,13 @@
+package com.dbserver.votacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicrediVotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicrediVotacaoApplication.class, args);
+	}
+
+}
