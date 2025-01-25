b6cc5115f11ed35d14f033a017c6597972dba588
ArthurVitor <arthurvitorgn@gmail.com>
2025-01-25 03:27:29 -0300

    feat: mapstruct implementation

--- src/main/java/com/dbserver/votacao/mapper/PautaMapper.java
+++ src/main/java/com/dbserver/votacao/mapper/PautaMapper.java
@@ -1,20 +1,20 @@
 package com.dbserver.votacao.mapper;
 
+import com.dbserver.votacao.dto.Page.PageDto;
 import com.dbserver.votacao.dto.Pauta.CreatePautaDto;
 import com.dbserver.votacao.dto.Pauta.ListPautaDto;
 import com.dbserver.votacao.model.Pauta;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.data.domain.Page;
 
-public class PautaMapper {
-    public static Pauta toPauta(CreatePautaDto dto) {
-        Pauta pauta = new Pauta();
-        pauta.setTitle(dto.title());
-        pauta.setDescription(dto.description());
-        pauta.setActive(dto.active());
-        pauta.setLifeTime(dto.lifeTime());
-        return pauta;
-    }
+@Mapper(componentModel = "spring")
+public interface PautaMapper {
+    ListPautaDto toDto(Pauta pauta);
 
-    public static ListPautaDto toDto(Pauta pauta) {
-        return new ListPautaDto(pauta.getId(), pauta.getTitle(), pauta.getDescription(), pauta.getActive(), pauta.getLifeTime());
-    }
+    Pauta toPauta(CreatePautaDto dto);
+
+    @Mapping(target = "pageNumber", source = "page.number")
+    @Mapping(target = "pageSize", source = "page.size")
+    PageDto<ListPautaDto> toPageDto(Page<ListPautaDto> page);
 }
